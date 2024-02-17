5601bcb4c82e9677ba583058f993089acb78ee6b
Lucca Zogbi <originlucca@hotmail.com>
2024-02-17 12:16:59 -0300

    Terminado exerício "Esfera" da plataforma Beecrowd.

---
+++ Python/Exercícios/Esfera.py
@@ -0,0 +1,11 @@
+# Entrada do raio da esfera 
+
+Raio = float(input())
+
+#Calculo do volume da esfera, com base no raio
+
+Volume = (4./3) * 3.14159 * (Raio**3)
+
+# Saída de dados
+
+print(F"VOLUME = %.3F" %(Volume))
